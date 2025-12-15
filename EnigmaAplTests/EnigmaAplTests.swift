12faa5e32905b687dd40ea3b72893585f84a4bc9
jankampherbeek <jan@kampherbeek.com>
2025-12-15 11:25:30 +0100

    Initial Commit

---
+++ EnigmaAplTests/EnigmaAplTests.swift
@@ -0,0 +1,16 @@
+//
+//  EnigmaAplTests.swift
+//  EnigmaAplTests
+//
+//  Created by Jan Kampherbeek on 15/12/2025.
+//
+
+import Testing
+
+struct EnigmaAplTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
