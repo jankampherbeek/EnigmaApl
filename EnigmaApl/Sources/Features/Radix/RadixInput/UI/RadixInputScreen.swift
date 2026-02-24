9f2f9914b84a16acbe8a0de8718c548606ccc252
jankampherbeek <jan@kampherbeek.com>
2026-02-24 14:49:10 +0100

    Working version before using new navigation approach

---
+++ EnigmaApl/Sources/Features/Radix/RadixInput/UI/RadixInputScreen.swift
@@ -0,0 +1,18 @@
+//
+//  RadixInputScreen.swift
+//  EnigmaApl
+//
+//  Created by Jan Kampherbeek on 23/02/2026.
+//
+
+import SwiftUI
+
+struct RadixInputScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RadixInputScreen()
+}
