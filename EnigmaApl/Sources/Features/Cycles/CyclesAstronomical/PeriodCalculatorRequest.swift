8a0dcc649b649fc8a3cb979bbbcb25585c72ee8f
jankampherbeek <jan@kampherbeek.com>
2026-05-16 21:31:23 +0200

    Created PeriodCalculatorRequest

---
+++ EnigmaApl/Sources/Features/Cycles/CyclesAstronomical/PeriodCalculatorRequest.swift
@@ -0,0 +1,34 @@
+// PeriodCalculatorRequest.swift
+// EnigmaApl is open source. For more information see se_license.html and License, both at the root of the application.
+// Created by Jan Kampherbeek 2026
+
+import Foundation
+
+/// Request for a period-based astronomical calculation.
+/// Describes which factor to calculate, the interval between steps, the time range, and the coordinate to use.
+public struct PeriodCalculatorRequest {
+    /// The celestial factor to calculate.
+    public let Factor: Factors
+    /// Interval in days between successive calculations.
+    public let Interval: Double
+    /// Julian Day number for the start of the period.
+    public let JdStart: Double
+    /// Julian Day number for the end of the period.
+    public let JdEnd: Double
+    /// The coordinate to calculate for the factor.
+    public let Coordinate: Coordinates
+
+    public init(
+        Factor: Factors,
+        Interval: Double,
+        JdStart: Double,
+        JdEnd: Double,
+        Coordinate: Coordinates
+    ) {
+        self.Factor = Factor
+        self.Interval = Interval
+        self.JdStart = JdStart
+        self.JdEnd = JdEnd
+        self.Coordinate = Coordinate
+    }
+}
